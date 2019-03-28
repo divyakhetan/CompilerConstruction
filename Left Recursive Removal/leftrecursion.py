6dc8625279235189d08aaaf9887223fb40ecd0da
divyakhetan <divyakhetan1209@gmail.com>
2019-03-28 22:57:19 +0530

    indirect removal lemma 2

--- Left Recursive Removal/leftrecursion.py
+++ Left Recursive Removal/leftrecursion.py
@@ -4,9 +4,56 @@ import re
 # A -> bA'
 # A' -> eps|aA'
 
+f1 = open('inputformated.txt', 'w')
 f = open('input.txt', 'r')
 
-for lines in f.readlines():
+def replace(lines):
+	lines = lines.rstrip()
+	x = re.split(r'[->]+', lines)
+	leftside = x[0]
+	rightside = re.split(r'[|]+', x[1])
+
+	f1.write(lines + "\n")
+
+lineslist = f.readlines()
+for lineno in range(0, len(lineslist)):
+	lines = lineslist[lineno]
+	lines = lines.rstrip()
+	x = re.split(r'[->]+', lines)
+	leftside = x[0]
+	rightside = re.split(r'[|]+', x[1])
+	ans = ""
+	for ind in range(0, len(rightside)):
+		# print(rightside[ind])
+		starting = rightside[ind][0] 
+		eval = False
+		if(starting != leftside):
+			for j in range(lineno + 1, len(lineslist)):
+				if(lineslist[j][0] == starting):
+					eval = True
+					linescomp = lineslist[j]
+					linescomp = linescomp.rstrip()
+					xcomp = re.split(r'[->]+', linescomp)
+					leftsidecomp = xcomp[0]
+					rightsidecomp = re.split(r'[|]+', xcomp[1])
+					# print(rightsidecomp)
+					for k in range(0, len(rightsidecomp)):
+						ans += rightsidecomp[k] + rightside[ind][1:] + "|"
+				
+
+		if(eval == False):
+			ans += rightside[ind] + "|"
+	ans = ans[:-1]
+	finalans = leftside + "->" + ans
+	print(finalans)
+	f1.write(finalans + "\n")
+
+	
+
+f1.close()
+f1 = open('inputformated.txt', 'r')
+print("\n")
+for lines in f1.readlines():
 	lines = lines.rstrip()
 	x = re.split(r'[->]+', lines)
 	leftside = x[0]
@@ -27,7 +74,7 @@ for lines in f.readlines():
 		for j in rightside:
 			ans += j + "|"
 		ans = ans[:-1]
-		# print(ans)
+		print(ans)
 	else:
 		ans1 = ""
 		for j in beta:
